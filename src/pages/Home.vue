2d3e8b8b0c72c2897945037d5d7bd5a3bba17fb9
apm29 <yjw999wow@163.com>
2021-06-17 11:43:06 +0800

    feature(dialog): confirm/alert方法,编程式的对话框

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -12,22 +12,40 @@
     <v-btn block @click="resetInputValidation">RESET VALIDATION</v-btn>
     <v-btn block @click="resetInput">RESET</v-btn>
     <v-btn block @click="toAbort">Abort</v-btn>
+    <v-btn
+      block
+      @click="
+        notification.error(
+          'asdadas获取微乎其微企鹅号奥施康定很快就暗红色的123asdasdasdasdasdadas获取微乎其微企鹅号奥施康定很快就暗红色的123asdasdasdasdasdadas获取微乎其微企鹅号奥施康定很快就暗红色的123asdasdasdasd'
+        )
+      "
+      >error</v-btn
+    >
+    <v-btn block @click="notification.success(123)">success</v-btn>
+    <v-btn block @click="notification.warning(123)">warning</v-btn>
+    <v-btn block @click="notification.info(123)">info</v-btn>
+    <v-btn block @click="testDialog">program-dialog</v-btn>
+    <v-btn block @click="testDialog2">program-dialog2</v-btn>
   </div>
 </template>
 
 <script>
 import VerifyCodeField from "@/components/VerifyCodeField.vue";
-import { useRouter } from "vue-router";
+import { notification } from "@/utils/notification/notify";
+import { dialog } from "@/utils/dialog/dialog";
 export default {
   name: "Home",
   components: { VerifyCodeField },
   data: () => ({
     code: "3333",
     validate: false,
+    show: false,
     rules: [
       (v) => Boolean(v) || "验证码不能为空",
       (v) => v.length === 6 || "必须为6位验证码",
     ],
+    notification,
+    dialog,
   }),
   methods: {
     validateInput() {
@@ -41,6 +59,16 @@ export default {
     },
     toAbort() {
       this.$router.push({ path: "/abort" });
+    },
+    async testDialog() {
+      if (await dialog.confirm("确认关闭吗?")) {
+        alert("已关闭");
+      }
+    },
+    async testDialog2() {
+      if (await dialog.alert("你好")) {
+        alert("已关闭");
+      }
     },
   },
 };
---
+++ src/utils/dialog/Dialogs.vue
@@ -0,0 +1,50 @@
+<template>
+  <teleport to="body">
+    <v-overlay
+      absolute
+      :modelValue="dialogStore.show"
+      @update:modelValue="onClick()"
+    >
+      <v-dialog-bottom-transition>
+        <v-card color="white" v-if="dialogStore.show">
+          <v-card-text class="tw-px-12 tw-py-4">
+            {{ dialogStore.message }}
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer />
+            <v-btn
+              v-if="dialogStore.type === 'confirm'"
+              small
+              text
+              @click="dialogStore.cancel()"
+            >
+              取消
+            </v-btn>
+            <v-btn small color="primary" @click="dialogStore.confirm()">
+              确认
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog-bottom-transition>
+    </v-overlay>
+  </teleport>
+</template>
+
+<script>
+import { dialogStore } from "./dialog";
+export default {
+  name: "Dialogs",
+  data: () => ({
+    dialogStore,
+  }),
+  methods: {
+    onClick(value) {
+      if (dialogStore.type === "alert" && !value) {
+        dialogStore.cancel();
+      }
+    },
+  },
+};
+</script>
+
+<style scoped></style>
