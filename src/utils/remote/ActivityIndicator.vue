5ebe4b03038ab4279fc4c0ddcfd99ae91e74b8b3
apm29 <yjw999wow@163.com>
2021-06-24 16:27:25 +0800

    feature(loading):活动指示器

--- src/utils/remote/ActivityIndicator.vue
+++ src/utils/remote/ActivityIndicator.vue
@@ -13,23 +13,54 @@
         tw-justify-center
         tw-items-center
         tw-bg-gray-500
-        tw-opacity-50
+        tw-bg-opacity-70
       "
     >
+      <div class="tw-relative tw-w-3/4">
+        <img alt="cloud" :src="cloud" class="tw-w-full" />
+        <img
+          alt="airplane"
+          :src="airplane"
+          class="
+            tw-w-1/2 tw-absolute tw-transform tw-left-0 tw-top-0
+            animation-fly-circle
+          "
+        />
+      </div>
       <div
         v-for="(entry, index) of store.app.token.entries()"
         :key="index"
-        class="tw-flex task-item tw-text-lg"
+        class="
+          tw-flex
+          task-item
+          tw-text-lg
+          tw-p-2
+          tw-bg-white
+          tw-rounded-lg
+          tw-items-center
+          tw-ring-1
+          tw-ring-sky-500
+          tw-shadow-lg
+          tw-mb-2
+        "
       >
-        <v-icon class="tw-w-6 tw-mr-4 tw-text-black">
+        <v-icon class="tw-text-2xl tw-mr-4 tw-text-green-600 tw-flex-shrink-0">
           mdi-spin mdi-loading
         </v-icon>
-        <div>{{ entry[0] }}</div>
-        <i
-          class="tw-w-6 tw-ml-4 mdi mdi-close hover:tw-text-red-500"
+        <div
+          class="
+            tw-text-gray-600 tw-text-left tw-font-bold tw-flex-grow
+            max-line-3
+          "
+        >
+          {{ entry[0] }}
+        </div>
+        <div
+          class="tw-text-sm tw-ml-4 tw-text-red-500 tw-flex-shrink-0"
           @click="entry[1].cancel('用户取消')"
         >
-        </i>
+          取消任务
+        </div>
       </div>
       <div class="tw-text-green-300 tw-mt-4">Loading...</div>
     </div>
@@ -38,15 +69,54 @@
 
 <script>
 import { store } from "./store";
+import airplane from "./assets/airplane.svg";
+import cloud from "./assets/cloud.svg";
 
 export default {
   name: "ActivityIndicator",
   setup() {
     return {
       store,
+      airplane,
+      cloud,
     };
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+@keyframes fly-circle {
+  0% {
+    left: 0;
+    top: 25%;
+  }
+  25% {
+    top: 0;
+  }
+  50% {
+    left: 50%;
+    top: 25%;
+  }
+  75% {
+    top: 50%;
+  }
+  100% {
+    left: 0;
+    top: 25%;
+  }
+}
+.animation-fly-circle {
+  animation: 4s linear 0s infinite fly-circle;
+}
+.task-item {
+  width: 75%;
+}
+.max-line-3 {
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 3;
+  overflow: hidden;
+  line-height: 1.25rem;
+  max-height: 3.75rem;
+}
+</style>
