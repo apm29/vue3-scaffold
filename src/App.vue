b2f480415bd0bf918c0bb8ee06b66ede7aa5409c
apm29 <yjw999wow@163.com>
2021-06-24 13:03:28 +0800

    fix(vite): 路径别名

--- src/App.vue
+++ src/App.vue
@@ -3,78 +3,20 @@
     <div class="tw-text-3xl tw-text-red-600 tw-font-extrabold">123</div>
   </div>
   <div>
-    <label>
-      <input
-        :value="captcha[index]"
-        class="input-cell"
-        v-for="(value, index) in 6"
-        :key="value"
-        @keydown="(event) => onAnyKeyDown(index, event)"
-        maxlength="1"
-        ref="input"
-        autocapitalize="characters"
-      />
-    </label>
+    <VerifyCodeField v-model:value="code"></VerifyCodeField>
   </div>
-  {{ captchaStr }}
+  {{ code }}
+  <v-text-field v-model="code"></v-text-field>
+  <v-btn>123</v-btn>
 </template>
 
 <script>
-import { reactive, computed } from "vue";
-
+import VerifyCodeField from "@/components/VerifyCodeField.vue";
 export default {
   name: "App",
-  setup(props) {
-    let captcha = reactive([]);
-    let captchaStr = computed(() =>
-      captcha.filter((it) => it !== undefined && it !== null).join("")
-    );
-
-    let onAnyKeyDown = function (index, event) {
-      console.log(event);
-      let input = event.target;
-      if (captcha[index] !== event.key && event.key.length === 1) {
-        captcha[index] = event.key;
-        event.preventDefault();
-      }
-
-      if (event.key === "Backspace") {
-        captcha[index] = null;
-        if (input.previousSibling && input.previousSibling.focus) {
-          input.previousSibling.focus();
-        }
-        event.preventDefault();
-      }
-
-      if (
-        event.target.nextSibling &&
-        event.target.nextSibling.focus &&
-        event.key !== "Backspace" &&
-        event.key !== "Tab"
-      ) {
-        event.target.nextSibling.focus();
-      }
-    };
-    return {
-      captcha,
-      captchaStr,
-      onAnyKeyDown,
-    };
-  },
+  components: { VerifyCodeField },
+  data: () => ({
+    code: undefined,
+  }),
 };
 </script>
-<style scoped>
-.input-cell {
-  @apply tw-w-12 tw-h-12
-  tw-shadow-lg
-  tw-rounded-lg
-  tw-border-8
-  tw-m-2 tw-p-2
-  tw-ring-1 tw-ring-gray-400 tw-outline-none
-  tw-text-center
-  tw-font-light
-  tw-text-4xl
-  tw-uppercase
-  focus:tw-outline-none  focus:tw-shadow-2xl focus:tw-ring-2 focus:tw-ring-purple-500 focus:tw-border-trueGray-50;
-}
-</style>
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+const { resolve } = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,4 +19,11 @@ export default defineConfig({
     ]
   },
   */
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
 });
+
+console.log(resolve(__dirname, "src"));
