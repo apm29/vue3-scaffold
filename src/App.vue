8f83ed1718ce45ac82013be7bace14357daca60d
apm29 <yjw999wow@163.com>
2021-06-24 13:33:07 +0800

    fix(notification):后缀.vue

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,13 @@
       </keep-alive>
     </v-slide-x-transition>
     <Notification></Notification>
+    <Dialogs />
   </router-view>
 </template>
 <script>
 import Notification from "@/utils/notification/Notification.vue";
+import Dialogs from "@/utils/dialog/Dialogs.vue";
 export default {
-  components: { Notification },
+  components: { Dialogs, Notification },
 };
 </script>
