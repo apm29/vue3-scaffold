a851d525b578892c955d54dc1690e2ccd58f994f
apm29 <yjw999wow@163.com>
2021-06-24 13:21:41 +0800

    fix(router): 为router新增keepalive和过渡动画

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <router-view v-slot="{ Component }">
+  <router-view v-slot="{ Component }" class="tw-h-full">
     <v-slide-x-transition>
       <keep-alive>
-        <component :is="Component" />
+        <component :is="Component" class="tw-h-full" />
       </keep-alive>
     </v-slide-x-transition>
   </router-view>
