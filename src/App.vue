6f11c7630780655db75448d3c4220ba23a9fe4f0
apm29 <yjw999wow@163.com>
2021-06-24 13:19:01 +0800

    fix(router): 引入vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,11 @@
 <template>
-  <div class="tw-bg-gradient-to-r tw-from-amber-500 tw-to-purple-300">
-    <div class="tw-text-3xl tw-text-red-600 tw-font-extrabold">123</div>
-  </div>
-  <div>
-    <VerifyCodeField v-model:value="code"></VerifyCodeField>
-  </div>
-  {{ code }}
-  <v-text-field v-model="code"></v-text-field>
-  <v-btn>123</v-btn>
+  <RouterView />
 </template>
-
 <script>
-import VerifyCodeField from "@/components/VerifyCodeField.vue";
+import { RouterView } from "vue-router";
 export default {
-  name: "App",
-  components: { VerifyCodeField },
-  data: () => ({
-    code: undefined,
-  }),
+  components: {
+    RouterView,
+  },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from "vue";
-import vuetify from "./plugins/vuetify";
-import App from "./App.vue";
-import "./index.css";
+import vuetify from "@/plugins/vuetify";
+import router from "@/router/router";
+import App from "@/App.vue";
+import "@/index.css";
 const app = createApp(App);
 app.use(vuetify);
+app.use(router);
 
 app.mount("#app");
