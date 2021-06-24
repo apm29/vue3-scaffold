5e7025f21a7e1d95ad511187b77d01e0d0adf93e
apm29 <yjw999wow@163.com>
2021-06-24 13:28:32 +0800

    fix(vuex):引入vuex

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from "vue";
 import vuetify from "@/plugins/vuetify";
 import router from "@/router/router";
+import store from "@/store";
 import App from "@/App.vue";
 import "@/index.css";
 const app = createApp(App);
 app.use(vuetify);
 app.use(router);
+app.use(store);
 
 app.mount("#app");
